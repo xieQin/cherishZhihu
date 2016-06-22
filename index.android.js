db195d02dac4553617a9a0d254e108b80e96124d
cherishhsieh <xieqin1991@gmail.com>
2016-06-22 10:35:45 +0800

    add flexbox and styles

--- index.android.js
+++ index.android.js
@@ -10,6 +10,7 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
   ScrollView,
   TouchableHighlight,
   DrawerLayoutAndroid,
@@ -27,9 +28,46 @@ class cherishZhihu extends Component {
             <Text>Proper Touch Handling</Text>
           </TouchableHighlight>
         </ScrollView>
+        <View style={styles.row}>
+          <Image
+            source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            style={styles.image}
+          />
+          <View style={styles.text}>
+            <Text style={styles.title}>
+              React Native
+            </Text>
+            <Text style={styles.subtitle}>
+              Build high quality mobile apps using React
+            </Text>
+          </View>
+        </View>
       </DrawerLayoutAndroid>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    margin: 40
+  },
+  image: {
+    width: 40,
+    height: 40,
+    marginRight: 10
+  },
+  text: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 11,
+    fontWeight: 'bold'
+  },
+  subtitle: {
+    fontSize: 10
+  }
+})
 
 AppRegistry.registerComponent('cherishZhihu', () => cherishZhihu);
