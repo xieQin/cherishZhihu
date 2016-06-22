393a99ef2a251420f12064fe470350ee48e6d137
cherishhsieh <xieqin1991@gmail.com>
2016-06-22 14:44:17 +0800

    add animations

--- index.android.js
+++ index.android.js
@@ -14,67 +14,102 @@ import {
   Image,
   ScrollView,
   ListView,
+  Animated,
   TouchableHighlight,
   DrawerLayoutAndroid,
   ProgressBarAndroid
 } from 'react-native';
 
 class cherishZhihu extends Component {
-  constructor(props) {
+  constructor(props: any) {
     super(props)
-    let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
     this.state = {
-      position: 'unknown',
-      dataSource: ds.cloneWithRows(['John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie'])
+      bounceValue: new Animated.Value(0),
     }
-  }
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({position}),
-      (error) => console.log(error)
-    )
   }
   render() {
     return (
-      <DrawerLayoutAndroid
-        renderNavigationView={() => <Text>React Native</Text>}>
-        <ProgressBarAndroid/>
-        <ScrollView style={styles.container}>
-          <TouchableHighlight
-            onPress={() => console.log('pressed')}>
-            <Text style={styles.item}>Position: {JSON.stringify(this.state.position)}</Text>
-          </TouchableHighlight>
-          <TextInput style={styles.item} placeholder='Hello~'/>
-        </ScrollView>
-        <View style={styles.container}>
-          <ListView
-            dataSource={this.state.dataSource}
-            renderRow={(rowData) =>
-              <TouchableHighlight
-                onPress={() => console.log('listView pressed')}>
-                <Text style={styles.item}>{rowData}</Text>
-              </TouchableHighlight>
-            }
-          />
-        </View>
-        <View style={styles.row}>
-          <Image
-            source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
-            style={styles.image}
-          />
-          <View style={styles.text}>
-            <Text style={styles.title}>
-              React Native
-            </Text>
-            <Text style={styles.subtitle}>
-              Build high quality mobile apps using React
-            </Text>
-          </View>
-        </View>
-      </DrawerLayoutAndroid>
-    );
+      <Animated.Image                         // Base: Image, Text, View
+        source={{uri: 'http://i.imgur.com/XMKOH81.jpg'}}
+        style={{
+          flex: 1,
+          transform: [                        // `transform` is an ordered array
+            {scale: this.state.bounceValue},  // Map `bounceValue` to `scale`
+          ]
+        }}
+      />
+    )
+  }
+  componentDidMount() {
+    this.state.bounceValue.setValue(1.5)      // Start large
+    Animated.spring(                          // Base: spring, decay, timing
+      this.state.bounceValue,                 // Animate `bounceValue`
+      {
+        toValue: 0.8,                         // Animate to smaller size
+        friction: 1,                          // Bouncier spring
+      }
+    ).start()                                 // Start the animation
   }
 }
+
+// class cherishZhihu extends Component {
+//   constructor(props) {
+//     super(props)
+//     let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+//     this.state = {
+//       position: 'unknown',
+//       dataSource: ds.cloneWithRows(['John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie'])
+//     }
+//   }
+//   componentDidMount() {
+//     navigator.geolocation.getCurrentPosition(
+//       (position) => this.setState({position}),
+//       (error) => console.log(error)
+//     )
+//   }
+//   render() {
+//     return (
+//       <DrawerLayoutAndroid
+//         renderNavigationView={() => <Text>React Native</Text>}>
+//         <ProgressBarAndroid/>
+//         <ScrollView style={styles.container}>
+//           <TouchableHighlight
+//             style={styles.item}
+//             onPress={() => console.log('pressed')}>
+//             <Text>Position: {JSON.stringify(this.state.position)}</Text>
+//           </TouchableHighlight>
+//           <TextInput style={styles.item} placeholder='Hello~'/>
+//         </ScrollView>
+//         <View style={styles.container}>
+//           <ListView
+//             dataSource={this.state.dataSource}
+//             renderRow={(rowData) =>
+//               <TouchableHighlight
+//                 style={styles.item}
+//                 onPress={() => console.log('listView pressed')}>
+//                 <Text>{rowData}</Text>
+//               </TouchableHighlight>
+//             }
+//           />
+//         </View>
+//         <View style={styles.row}>
+//           <Image
+//             source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+//             style={styles.image}
+//           />
+//           <View style={styles.text}>
+//             <Text style={styles.title}>
+//               React Native
+//             </Text>
+//             <Text style={styles.subtitle}>
+//               Build high quality mobile apps using React
+//             </Text>
+//           </View>
+//         </View>
+//       </DrawerLayoutAndroid>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -85,7 +120,8 @@ const styles = StyleSheet.create({
     lineHeight: 40,
     justifyContent: 'center',
     marginLeft: 40,
-    marginRight: 40
+    marginRight: 40,
+    textAlign: 'left'
   },
   row: {
     flexDirection: 'row',
